f5c7380fe3646c88198b251e973e18e36d0a2ace
Pere Urbon-Bayes <pere.urbon@gmail.com>
2020-11-20 11:32:20 +0100

    streamline the ccloud cli flow for service account and env settling

--- src/main/java/com/purbon/kafka/topology/principals/CCloudCLI.java
+++ src/main/java/com/purbon/kafka/topology/principals/CCloudCLI.java
@@ -6,79 +6,106 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.Map;
 
 public class CCloudCLI {
 
   private static final Logger LOGGER = LogManager.getLogger(CCloudCLI.class);
 
   private ObjectMapper mapper;
-  private Runtime run;
 
   public CCloudCLI() {
     mapper = new ObjectMapper();
-    run = Runtime.getRuntime();
   }
 
   public List<Cluster> clusters() throws IOException {
+    List<String> cmd = Arrays.asList("ccloud", "kafka", "cluster", "list", "--output", "json");
+    String stdout = "";
     try {
-      String stdout = run("ccloud kafka cluster list --output json");
+      stdout = run(cmd);
       Cluster[] items = mapper.readValue(stdout, Cluster[].class);
       return Arrays.asList(items);
     } catch (IOException | InterruptedException e) {
-      LOGGER.error("Something happen during calling ccloud", e);
-      throw new IOException(e);
+      handleError(stdout, e);
     }
+    return null;
   }
 
   public Map<String, ServiceAccount> serviceAccounts() throws IOException {
+    List<String> cmd = Arrays.asList("ccloud", "service-account", "list", "--output", "json");
+    String stdout = "";
     try {
-      String stdout = run("ccloud service-account list --output json");
+      stdout = run(cmd);
       ServiceAccount[] items = mapper.readValue(stdout, ServiceAccount[].class);
       return Arrays.asList(items).stream().collect(Collectors.toMap(i -> ""+i.getId(), i->i));
     } catch (IOException | InterruptedException e) {
-      LOGGER.error("Something happen during calling ccloud", e);
-      throw new IOException(e);
+      handleError(stdout, e);
+      return null;
     }
   }
 
-  private ServiceAccount newServiceAccount(String name, String description) throws IOException {
-    //String cmd = "ccloud service-account create "+name+" --description \""+description+"\" --output json 2>&1";
-    try {
-      ProcessBuilder pb = new ProcessBuilder("ccloud", "service-account", "create "+name, "--description", description, "--output json");
-      Process pr = pb.start();
-      pr.waitFor();
-      String stdout = readStdOut(pr);
-      ServiceAccount sa = mapper.readValue(stdout, ServiceAccount.class);
-      return sa;
-    } catch (IOException | InterruptedException e) {
-      LOGGER.error("Something happen during calling ccloud", e);
-      throw new IOException(e);
-    }
-  }
 
   public void setEnvironment(String environment) throws IOException {
+    List<String> cmd = Arrays.asList("ccloud", "environment", "use", environment);
+    String stdout = "";
     try {
-      run("ccloud environment use " + environment);
+      stdout = run(cmd);
     } catch (IOException | InterruptedException e) {
-      LOGGER.error("Something happen during setting an environment", e);
-      throw new IOException(e);
+      handleError(stdout, e);
     }
   }
 
-  private String run(String cmd) throws IOException, InterruptedException {
-    Process pr = run.exec(cmd);
+
+  public ServiceAccount newServiceAccount(String name, String description) throws IOException {
+    List<String> cmd = Arrays.asList("ccloud", "service-account", "create", name, "--description", description, "--output", "json");
+    String stdout = "";
+    ServiceAccount sa = null;
+    try {
+      stdout = run(cmd);
+      sa = mapper.readValue(stdout, ServiceAccount.class);
+    } catch (IOException | InterruptedException e) {
+      handleError(stdout, e);
+    }
+    return sa;
+  }
+
+  public void deleteServiceAccount(int id) throws IOException {
+    List<String> cmd = Arrays.asList("ccloud", "service-account", "delete", String.valueOf(id));
+    String stdout = "";
+    try {
+      stdout = run(cmd);
+    } catch (IOException | InterruptedException e) {
+      handleError(stdout, e);
+    }
+  }
+
+  private void handleError(String stdout, Exception e) throws IOException {
+    String errorMsg = String.format("Something happen with ccloud. \n %s", stdout);
+    LOGGER.error(errorMsg, e);
+    throw new IOException(e);
+  }
+
+  private String run(List<String> cmd) throws IOException, InterruptedException {
+
+    ProcessBuilder builder = new ProcessBuilder();
+    builder.command(cmd);
+    builder.redirectErrorStream(true);
+
+    Process pr = builder.start();
+    String stdout = readStdOut(pr);
     pr.waitFor();
-    return readStdOut(pr);
+
+    LOGGER.debug("Exit code: "+pr.exitValue());
+    return stdout;
   }
 
   private String readStdOut(Process pr) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(pr.getInputStream()));
     StringBuilder sb = new StringBuilder();
-    String line = "";
+    String line = null;
     while ((line = br.readLine()) != null) {
      sb.append(line);
      sb.append("\n");
@@ -86,12 +113,11 @@ public class CCloudCLI {
     return sb.toString();
   }
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
+
     CCloudCLI cli = new CCloudCLI();
     cli.setEnvironment("env-j9wgp");
 
-    ServiceAccount sa = cli.newServiceAccount("ktb", "created by KTB");
-    System.out.println(sa);
 
   }
 }
